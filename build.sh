5a631b03a0ebeaa26bcf850f06aa6b27d021ed0a
Rok Kos <kosrok97@gmail.com>
2024-03-19 07:47:45 +0100

    feat(compile): warning are error

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ SOURCE_FILE="main.c"
 OUTPUT_FILE="mmel"
 
 
-WARNINGS=" -Wall -Wextra -Wpedantic"
-CFLAGS="-std=c99 -O3"
+WARNINGS=" -Wall -Wextra -Wpedantic -Werror"
+CFLAGS="-std=c99 -O3 -fstack-protector-strong -fsanitize=address -fsanitize=undefined"
 
 clang $WARNINGS $CFLAGS -o $OUTPUT_FILE $SOURCE_FILE -v 
 
